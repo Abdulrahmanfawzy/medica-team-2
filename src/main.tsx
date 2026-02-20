e9d3551cb7336253c65c74f685283c0390002c27
Omar Elbastawesy <omarelbastawesy2@gmail.com>
2026-02-20 18:54:12 +0200

    some edits folders and some files

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,10 @@
-import DataProvider from "./hooks/usecontext.tsx";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-// import "@fontsource/poppins/400.css";
-// import "@fontsource/poppins/500.css";
-// import "@fontsource/poppins/600.css";
-// import "@fontsource/poppins/700.css";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
-
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-);
+)
