bfd3597ef74712f04249070cda6b235244aa712d
Mohammed <mohamadazizo988@gmail.com>
2026-02-21 12:45:50 +0200

    make favorite page style

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Favorite from "./pages/Favorite";
 import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="px-24">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorite />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 };
 
---
+++ src/pages/CardFavorite.tsx
@@ -0,0 +1,98 @@
+import { MapPin, Star, Clock, Heart, MessageSquare } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+
+interface DoctorProps {
+  name: string;
+  specialty: string;
+  rating: number;
+  experience: number;
+  location: string;
+  imageUrl: string;
+}
+
+function DoctorCard({
+  name,
+  specialty,
+  rating,
+  experience,
+  location,
+  imageUrl,
+}: DoctorProps) {
+  return (
+    <Card className="overflow-hidden mb-4 border-slate-200">
+      <CardContent className="p-4 flex flex-col md:flex-row justify-center items-center gap-6">
+        {/* Doctor Image */}
+        <div className="w-full md:w-48 h-48 rounded-lg overflow-hidden bg-slate-100 flex-shrink-0">
+          <img
+            src={imageUrl}
+            alt={name}
+            className="w-full h-full object-cover"
+          />
+        </div>
+
+        {/* Info Section */}
+        <div className="flex-1 space-y-2">
+          <div className="flex justify-between items-start">
+            <div>
+              <h3 className="text-xl font-bold text-teal-700">Dr. {name}</h3>
+              <p className="text-slate-500 font-medium">{specialty}</p>
+            </div>
+            <div className="p-3 border border-slate-200 rounded-xl ">
+              <Heart
+                className="heart text-slate-400 cursor-pointer  hover:text-red-500 transition-colors"
+                onClick={() => {}}
+                size={24}
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center gap-1 text-yellow-500">
+            {[...Array(5)].map((_, i) => (
+              <Star
+                key={i}
+                size={16}
+                fill={i < Math.floor(rating) ? "currentColor" : "none"}
+              />
+            ))}
+            <span className="text-slate-600 font-semibold ml-1">{rating}</span>
+          </div>
+
+          <div className="flex md:flex-row flex-col justify-between items-center gap-1 text-slate-500 text-sm">
+            <div className="flex items-center gap-2">
+              <MapPin size={16} /> {location}
+            </div>
+            <div className="flex items-center gap-2">
+              <Clock size={16} /> {experience} years experience
+            </div>
+          </div>
+
+          <Button className="bg-white hover:bg-white hover:text-teal-700 px-6 border border-teal-700 text-teal-700">
+            Available Today
+          </Button>
+
+          {/* Actions */}
+          <div className="flex flex-wrap gap-2 pt-4">
+            <Button
+              variant={"outline"}
+              className="bg-white hover:bg-teal-700 hover:text-white px-6 border-teal-700 text-teal-700 cursor-pointer">
+              Book Appointment
+            </Button>
+            <Button
+              variant="outline"
+              className="bg-white hover:bg-teal-700 hover:text-white px-6 border-teal-700 text-teal-700 cursor-pointer">
+              View Profile
+            </Button>
+            <Button
+              variant="outline"
+              className="bg-white hover:bg-teal-700 hover:text-white px-6 border-teal-700 text-teal-700  cursor-pointer">
+              Message
+            </Button>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
+export default DoctorCard;
