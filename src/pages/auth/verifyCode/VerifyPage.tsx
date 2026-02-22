156ff2fdbb3d36af723b85c527adfe11b79c7432
Eslam-OP <123605401+Eslam-OP@users.noreply.github.com>
2026-02-22 19:38:45 +0200

    editing code

--- src/pages/auth/verifyCode/VerifyPage.tsx
+++ src/pages/auth/verifyCode/VerifyPage.tsx
@@ -76,7 +76,7 @@ export default function VerifyPage() {
                             <InputOTPSlot
                               key={i}
                               index={i}
-                              className="w-10 h-10 sm:w-16 sm:h-16 text-center text-lg sm:text-xl font-semibold rounded-[10px]! border-2 border-[#097178] bg-transparent focus:outline-none focus:ring-2 focus:ring-teal-600 transition"
+                              className="w-10 h-10 sm:w-14 sm:h-14 text-center text-lg sm:text-xl font-semibold rounded-[10px]! border-2 border-[#097178] bg-transparent focus:outline-none focus:ring-2 focus:ring-teal-600 transition"
                             />
                           ))}
                         </InputOTPGroup>
